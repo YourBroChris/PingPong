64b2bc37a233710fb3605751cc5459be5e179aed
Christoffer Johansen <94114586+YourBroChris@users.noreply.github.com>
2025-09-05 22:50:05 +0200

    Remove init_interrupts function and include
    Removed interrupt initialization function and its related include.

--- usart.c
+++ usart.c
@@ -1,14 +1,5 @@
 #include "usart.h"
 #include <avr/io.h>
-#include <avr/interrupt.h>
-
-
-void init_interrupts(void)
-{
-    // Enable global interrupts
-    SREG |= (1 << SREG_I);
-    
-}
 
 void init_usart(unsigned int ubrr)
 {
