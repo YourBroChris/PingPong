004bee630225df90862afb0c3745cb76e77ad966
Matheus Ullmann <matheusu@ntnu.no>
2025-09-18 12:48:55 +0200

    ADC

---
+++ adc.c
@@ -0,0 +1,15 @@
+#include "adc.h"
+
+void adc_init (){
+
+}
+uint8_t adc_read(uint8_t channel){
+    return 0;
+} //volatile
+void pos_calibrate(){
+
+}
+
+pos_t pos_read(){
+    return (pos_t){0,0};
+}
---
+++ adc.h
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <avr/io.h>
+
+struct pos_t{
+    uint8_t x;
+    uint8_t y;
+};
+typedef struct pos_t pos_t;
+
+void adc_init (void);
+uint8_t adc_read(uint8_t channel); //volatile
+void pos_calibrate();
+pos_t pos_read(void);
+
