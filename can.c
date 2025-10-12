f9757568ed9b7cd955d7ff206906c3daecb45cd5
Jens Bergem <jensfbe@stud.ntnu.no>
2025-10-12 16:43:17 +0200

    added read and write instructions

--- can.c
+++ can.c
@@ -1,1 +1,34 @@
 #include "can.h"
+#include <avr/io.h>
+#include "spi.h"
+
+
+void can_init(){
+    DDRB |= (1 << PB4);
+    slave_select(NONE);
+}
+
+void read_instruction(uint8_t addr){
+    slave_select(CAN);
+    SPDR = (uint8_t)0b00000011; // READ instruction = 00000011
+    // Wait for transmission complete 
+    while(!(SPSR & (1<<SPIF)));
+    SPDR = addr;
+    // Wait for transmission complete 
+    while(!(SPSR & (1<<SPIF)));
+    slave_select(NONE);
+}
+
+void write_instruction(uint8_t addr, uint8_t data){
+    slave_select(CAN);
+    SPDR = (uint8_t)0b00000010; // READ instruction = 00000011
+    // Wait for transmission complete 
+    while(!(SPSR & (1<<SPIF)));
+    SPDR = addr;
+    // Wait for transmission complete 
+    while(!(SPSR & (1<<SPIF)));
+    SPDR = data;
+    while(!(SPSR & (1<<SPIF)));
+    // slave_select(NONE); Ikke dra opp CS før man er ferdig med å skrive, kan fortsette å skrive
+}
+
--- can.h
+++ can.h
@@ -3,3 +3,10 @@ typedef struct {
     unsigned char length;
     char data[8];
 } can_message
+
+
+typedef enum {
+    RESET = 0b11000000,
+    READ = 0b00000011,
+    WRITE = 0b00000010
+} can_instruction
